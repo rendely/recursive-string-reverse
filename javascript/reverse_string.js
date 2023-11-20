c5a8d790b3f0c5971ade24b70098650b7d0383ec
matthew rendely <matthew.rendely@gmail.com>
2023-11-19 20:59:00 -0800

    Done

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
-  // type your code here
+  if (str.length <= 1) return str;
+
+  const mid = Math.floor(str.length / 2);
+
+  return reverseString(str.slice(mid)) + reverseString(str.slice(0,mid))
+  
 }
 
 if (require.main === module) {
@@ -17,3 +22,12 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/*
+
+If 1 character or no characters, return as-is
+If 2 characters you swap them
+If 3 characters you treat the first 2 characters first, and then swap that result with the next character
+If 4 characters you swap the first 2, swap the last 2, and then swap those two sets
+  help => eh, pl => pleh
+
+*/
